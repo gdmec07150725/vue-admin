f09bc1844604b88c38c454d8aafdb8a6a6962a68
liaonan <liaonan@mosainet.com>
2019-02-18 11:17:34 +0800

    第七课--vuex进阶

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,17 @@ import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
 import user from './module/user'
-
+import saveInLocal from './plugin/saveInLocal'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict:  process.env.NODE_ENV === 'development', //开发模式下使用严格模式
   state,
   getters,
   mutations,
   actions,
   modules: {
     user
-  }
+  },
+  plugins:[ saveInLocal ]
 })
